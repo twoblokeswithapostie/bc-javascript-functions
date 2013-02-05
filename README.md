c3f3307f059cb4cedc2b75ca3ae3bc5f41df0263
Mario Gudelj <mariogudelj@pythonic.(none)>
2013-02-05 11:01:32 +1100

    added more functions

--- README.md
+++ README.md
@@ -15,3 +15,55 @@ You need to add jQuery to the head of your document before linking this file. He
 ```html
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 ```
+
+Functions
+-------------
+
+### function makeShippingSameAsBilling()
+
+Usage: 
+
+Use this function to populate the the shipping address field with the billing address fields on the Business Catalyst checkout form.
+
+To use this function simply add a checkbox to your checkout form and make an onclick call to the function e.g.
+
+```html
+<input type="checkbox" onclick="makeShippingSameAsBilling();" />
+```
+
+### function changeEmptyCartMessage()
+
+Usage:
+
+changeEmptyCartMessage() function is called when the shopping is empty. In our example the Ajax is used to retrieve a web page called empty_cart_page.htm. This allows you to create a rich empty cart page with catalogues, products, links and anything else you think will improve the shop functionality and increase conversions.
+
+### function UpdateProductExtras(c, p, res)
+
+Usage:
+
+Here we are overriding BC's UpdateProductExtras function and calling changeEmptyCartMessage. 
+
+### $.alertMessageHolder = function(note, messageStyle)
+
+Usage:
+
+alertMessageHolder is a function that embeds the message into body. You must add <div id="alertMessageHolder"><p></p></div> to the top of the shoppign cart layout and all the captured alerts will be displayed inside the page body instead of popping up and distracting the customer.
+
+The following is the function which captures alerts and passes them onto alertMessageHolder:
+
+```javascript
+window.alert = function(msg){
+    messageStyle = 'success'; // Set style to success. We'll set it to error later on if the message is the Error message. Done so you can apply different styles    
+    
+    
+    if ((msg.indexOf('ERROR:') >= 0) || (msg.indexOf('- ') >= 0))
+    {
+      messageStyle = 'error';  
+    }   
+    $.alertMessageHolder(msg, messageStyle);
+    return false;
+ }
+ ```
+  
+
+
--- functions.js
+++ functions.js
@@ -1,6 +1,72 @@
 
-/* Put together by http://www.twoblokeswithapostie.com */
-/* --------------------------------------------------- */
+/* Put together by http://www.twoblokeswithapostie.com 	*/
+/* --------------------------------------------------- 	*/
+/* Always rememberr to make calls like this:    			*/
+/* if($('#some-form-selector').length) {				*/ 
+/*	funtionCall();										*/ 
+/*  	}												*/ 
+
+
+
+
+$(document).ready(function($){
+
+// alertMessageHolder is a function that embeds the message into body. You must add
+// <div id="alertMessageHolder"><p></p></div> to the top of the shoppign cart layout.
+
+$.alertMessageHolder = function(note, messageStyle) {
+    
+    messageStyle = (messageStyle == 'error') ? 'alertMessageHolder_error' : 'alertMessageHolder_success'; 
+    
+    // messageStyle will render alertMessageHolder_error or alertMessageHolder_success. You need to use these class in your CSS to style the message box
+     
+    $('html').animate({ scrollTop: 0 }, 'slow', function(){
+      $('#alertMessageHolder').fadeOut('fast', function(){
+        $('#alertMessageHolder p').attr('class', messageStyle).html(note);
+        $('#alertMessageHolder').fadeIn('slow');
+      });
+    });
+  }
+
+// Here we're capturing the messages from those BC alerts
+
+window.alert = function(msg){
+    messageStyle = 'success'; // Set style to success. We'll set it to error later on if the message is the Error message. Done so you can apply different styles    
+    
+    
+    if ((msg.indexOf('ERROR:') >= 0) || (msg.indexOf('- ') >= 0))
+    {
+      messageStyle = 'error';  
+    }   
+    $.alertMessageHolder(msg, messageStyle);
+  }
+  return false;
+  
+}); 
+
+// Here we are overriding BC's UpdateProductExtras function and calling changeEmptyCartMessage
+
+function UpdateProductExtras(c, p, res) {
+        if (res == -1) {
+        changeEmptyCartMessage();
+        }
+}
+
+// changeEmptyCartMessage() function is called when the shopping is empty. In this example the Ajax is used to retrieve 
+// a web page called empty_cart_page.htm. This allows you to place anything you wish inside your message
+
+function changeEmptyCartMessage() {
+    var ajax_url = '/empty_cart_page.htm';
+    $.ajax({
+      url:     ajax_url,
+      data:    '',
+      success: function(output){
+        $('#catCartDetails .cartLink').remove();
+        $('#catCartDetails').html(output);
+      }
+    });
+}
+
 
 
 // To use this function simply add a checkbox to your checkout form and 
@@ -24,3 +90,6 @@ function makeShippingSameAsBilling(){
              $("#ShippingCountry").val($("#BillingCountry").val()); 
       }
  }
+
+//END .ready()
+});
